ae5ac37ff0f8b0271e4f6966c0f5cefc7fdbb258
Matthew Lund <50162103+mtlund@users.noreply.github.com>
2020-02-13 01:02:02 -0800

    Update uncompress.cpp

--- src/uncompress.cpp
+++ src/uncompress.cpp
@@ -12,10 +12,34 @@
 
 /* TODO: Pseudo decompression with ascii encoding and naive header (checkpoint)
  */
-void pseudoDecompression(string inFileName, string outFileName) {}
+void pseudoDecompression(string inFileName, string outFileName) {
+    vector<unsigned int> ofChars(256);
+    HCTree* ofAllChars = new HCTree();
+    ifstream ofInput;
+    unsigned char nextChar;
+    int nextByte;
+    ofInput.open(inFileName, ios::binary);
+    for (int i = 0; i < 256; i++) {
+        ofInput >> ofChars.at(i);
+    }
+    ofAllChars->build(ofChars);
+    ofstream ofOutput;
+    ofOutput.open(outFileName);
+    while ((nextByte = ofInput.peek()) != EOF) {
+        ofOutput << ofAllChars->decode(ofInput);
+    }
+    ofInput.close();
+    ofOutput.close();
+    delete ofAllChars;
+}
 
 /* TODO: True decompression with bitwise i/o and small header (final) */
 void trueDecompression(string inFileName, string outFileName) {}
 
 /* TODO: Main program that runs the uncompress */
-int main(int argc, char* argv[]) { return 0; }
+int main(int argc, char* argv[]) {
+    string ofInputFile = argv[2];
+    string ofOutputFile = argv[3];
+    pseudoDecompression(ofInputFile, ofOutputFile);
+    return 0;
+}
