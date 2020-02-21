d06c3aa4233a0d4c325df1fbfa320eb83ae8c89b
Matthew Lund <50162103+mtlund@users.noreply.github.com>
2020-02-20 21:00:41 -0800

    Add files via upload

--- src/bitStream/input/BitInputStream.cpp
+++ src/bitStream/input/BitInputStream.cpp
@@ -1,56 +1,118 @@
 /**
- * TODO: file header
+ * Authors: Matthew Lund and Mohammad Javid
+ * Contact info: mtlund@ucsd.edu (Matthew),
+ *               Msamadpo@ucsd.edu (Mohammad)
  *
- * Author:
+ * Sources used:
+ *  1) Stepik: Introduction to Data Structures (Fall 2016)
+ *     by Moshiri and Izhikevich (available at stepik.org)
+ *
+ *     Section: 7.4 - Step 6
+ *
+ * Description of File:
+ *  This file defines the neccessary methods to create a
+ *  bit buffer to which we can read data from. Once
+ *  all of the data has been read, we refill the buffer
+ *  it possible.
+ *
  */
 #include "BitInputStream.hpp"
 
-/* TODO */
-void BitInputStream::fill() { this->in.read(this->buf, this->bufSize); }
+const unsigned int SIZE_OF_CHAR = 8;
 
-/* TODO */
+/*
+ * This method returns true whenever the buffer is full and
+ * has read all the data or when the input file does not
+ * have any more input.
+ *
+ * Returns:
+ *  1) bool - True if no more input or all input
+ *            has been read.
+ *
+ */
 bool BitInputStream::atEndOfFile() {
     if (eof()) {
         return false;
     }
-    if (this->nbits == in.gcount() * 8 && (!this->in)) {
+
+    if (this->nbits == in.gcount() * SIZE_OF_CHAR && (!this->in)) {
         return true;
     } else if (this->in.gcount() == 0 && (!this->in)) {
         return true;
     } else {
-        if (this->nbits == this->bufSize * 8) {
+        if (this->nbits == this->bufSize * SIZE_OF_CHAR) {
             this->nbits = 0;
         }
         return false;
     }
 }
 
-/* Returns true when trying to read past the end of the file, false otherwise */
+/*
+ * This method read in data from the input stream and
+ * stores it into the buffer. It reads characters
+ * until the buffer has been filled or input stream
+ * is empty.
+ *
+ * Sources used:
+ *  1) Stepik: Introduction to Data Structures (Fall 2016)
+ *     by Moshiri and Izhikevich (available at stepik.org)
+ *     Section: 7.4 - Step 6
+ *
+ */
+void BitInputStream::fill() { this->in.read(this->buf, this->bufSize); }
+
+/*
+ * This method return a boolean representing
+ * whether the user has tried to read past
+ * the buffer when no more data is allowed
+ * to be inputed.
+ *
+ * Returns:
+ *  1) bool - True if user is trying to read empty data.
+ */
 bool BitInputStream::eof() { return eofBit; }
 
-/* TODO */
+/*
+ * This method returns an int representing
+ * the bit that has not yet been read.
+ * If the buffer has been completely read,
+ * the method tries to refill the buffer.
+ *
+ * Sources used:
+ *  1) Stepik: Introduction to Data Structures (Fall 2016)
+ *     by Moshiri and Izhikevich (available at stepik.org)
+ *
+ *     Section: 7.4 - Step 6
+ *
+ * Edge Cases:
+ *  1) The buffer has been read -> try to read in data.
+ *  2) The buffer is empty -> try to read in data.
+ *
+ * Retruns:
+ *  1) int - representing the bit at postion nbit.
+ */
 unsigned int BitInputStream::readBit() {
-    if (this->nbits == 0 || this->nbits == this->bufSize * 8) {
+    if (this->nbits == 0 || this->nbits == this->bufSize * SIZE_OF_CHAR) {
         this->fill();
     }
     if (atEndOfFile()) {
         this->eofBit = true;
         return 0;
     }
-    int indexOfArray = this->nbits / 8;
-    int indexOfChar = (this->nbits + 1) % 8;
-    if (this->nbits == (this->bufSize * 8) - 1) {
+    int indexOfArray = this->nbits / SIZE_OF_CHAR;
+    int indexOfChar = (this->nbits + 1) % SIZE_OF_CHAR;
+    if (this->nbits == (this->bufSize * SIZE_OF_CHAR) - 1) {
         unsigned char atIndex = this->buf[indexOfArray];
-        atIndex = atIndex << (8 - 1);
-        atIndex = atIndex >> (8 - 1);
+        atIndex = atIndex << (SIZE_OF_CHAR - 1);
+        atIndex = atIndex >> (SIZE_OF_CHAR - 1);
         unsigned int ofIndex = atIndex && 1;
         this->nbits++;
         return ofIndex;
     } else {
         unsigned char atIndex = this->buf[indexOfArray];
         if (indexOfChar == 0) {
-            atIndex = atIndex << (8 - 1);
-            atIndex = atIndex >> (8 - 1);
+            atIndex = atIndex << (SIZE_OF_CHAR - 1);
+            atIndex = atIndex >> (SIZE_OF_CHAR - 1);
             unsigned int ofIndex = atIndex && 1;
             this->nbits++;
             return ofIndex;
@@ -59,11 +121,15 @@ unsigned int BitInputStream::readBit() {
             atIndex = atIndex << (indexOfChar - 1);
             atIndex = atIndex >> (indexOfChar - 1);
         }
-        atIndex = atIndex >> (8 - indexOfChar);
+        atIndex = atIndex >> (SIZE_OF_CHAR - indexOfChar);
         unsigned int ofIndex = atIndex && 1;
         this->nbits++;
         return ofIndex;
     }
 }
 
+/*
+ * This is the deconstructor. It makes sure
+ * to free the memory used by the buffer.
+ */
 BitInputStream::~BitInputStream() { delete[] this->buf; }
--- src/bitStream/input/BitInputStream.hpp
+++ src/bitStream/input/BitInputStream.hpp
@@ -1,7 +1,24 @@
 /**
- * TODO: file header
+ * Authors: Matthew Lund and Mohammad Javid
+ * Contact info: mtlund@ucsd.edu (Matthew),
+ *               Msamadpo@ucsd.edu (Mohammad)
  *
- * Author:
+ * Sources used:
+ *  1) Stepik: Introduction to Data Structures (Fall 2016)
+ *     by Moshiri and Izhikevich (available at stepik.org)
+ *
+ *     Section: 7.4 - Step 6
+ *
+ * Sources used:
+ *  1) Stepik: Introduction to Data Structures (Fall 2016)
+ *     by Moshiri and Izhikevich (available at stepik.org)
+ *
+ * Description of File:
+ *  This file defines the neccessary methods to create a
+ *  bit buffer to which we can read data from. Once
+ *  all of the data has been read, we refill the buffer
+ *  it possible.
+ *
  */
 #ifndef BITINPUTSTREAM_HPP
 #define BITINPUTSTREAM_HPP
@@ -13,7 +30,20 @@ typedef unsigned char byte;
 
 using namespace std;
 
-/** TODO: class header */
+/**
+ * The BitInputStream class allows the user to create a
+ * bit buffer in order to store data in its encoded form.
+ * Each element of the data is represented by a sequence
+ * of bits all of which make up different characters in
+ * the buffer.
+ *
+ * Instance variables:
+ *  1) in - the input stream to used to read in data
+ *  2) buf - The character array to store the data
+ *  3) bufSize - The size of the character array
+ *  4) nbits - The position of the next unread bit.
+ *  5) eofbit - True if the input stream is empty
+ */
 class BitInputStream {
   private:
     istream& in;           // reference to the input stream to use
@@ -22,11 +52,30 @@ class BitInputStream {
     unsigned int nbits;    // number of bits that have been read from buf
     bool eofBit;           // true when trying to read past the end of the file
 
-    /* TODO: add function header */
+    /*
+     * This method returns true whenever the buffer is full and
+     * has read all the data or when the input file does not
+     * have any more input.
+     *
+     * Returns:
+     *  1) bool - True if no more input or all input
+     *            has been read.
+     *
+     */
     bool atEndOfFile();
 
   public:
-    /* TODO: add function header and implement */
+    /*
+     * This is the constructor. It initializes all of the
+     * instance variables to operate the buffer.
+     * Once the buffer has been created it also zeroes
+     * out all of the bits.
+     *
+     * Parameters:
+     *  1) is - The input stream from which to read data from
+     *  2) bufSize - The size we want the buffer to have.
+     *
+     */
     explicit BitInputStream(istream& is, unsigned int bufSize) : in(is) {
         this->bufSize = bufSize;
         this->buf = new char[bufSize];
@@ -35,15 +84,57 @@ class BitInputStream {
         this->eofBit = false;
     };
 
-    /* TODO: add function header */
+    /*
+     * This method read in data from the input stream and
+     * stores it into the buffer. It reads characters
+     * until the buffer has been filled or input stream
+     * is empty.
+     *
+     * Sources used:
+     *  1) Stepik: Introduction to Data Structures (Fall 2016)
+     *     by Moshiri and Izhikevich (available at stepik.org)
+     *
+     *     Section: 7.4 - Step 6
+     *
+     */
     void fill();
 
-    /* TODO: add function header */
+    /*
+     * This method return a boolean representing
+     * whether the user has tried to read past
+     * the buffer when no more data is allowed
+     * to be inputed.
+     *
+     * Returns:
+     *  1) bool - True if user is trying to read empty data.
+     */
     bool eof();
 
-    /* TODO: add function header */
+    /*
+     * This method returns an int representing
+     * the bit that has not yet been read.
+     * If the buffer has been completely read,
+     * the method tries to refill the buffer.
+     *
+     * Sources used:
+     *  1) Stepik: Introduction to Data Structures (Fall 2016)
+     *     by Moshiri and Izhikevich (available at stepik.org)
+     *
+     *     Section: 7.4 - Step 6
+     *
+     * Edge Cases:
+     *  1) The buffer has been read -> try to read in data.
+     *  2) The buffer is empty -> try to read in data.
+     *
+     * Retruns:
+     *  1) int - representing the bit at postion nbit.
+     */
     unsigned int readBit();
 
+    /*
+     * This is the deconstructor. It makes sure
+     * to free the memory used by the buffer.
+     */
     ~BitInputStream();
 };
 
