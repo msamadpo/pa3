058edb775e1c3316618c3ebadc844a1fa36c827f
Matthew Lund <50162103+mtlund@users.noreply.github.com>
2020-02-20 21:01:08 -0800

    Add files via upload

--- src/bitStream/output/BitOutputStream.cpp
+++ src/bitStream/output/BitOutputStream.cpp
@@ -1,51 +1,122 @@
 /**
- * TODO: file header
+ * Authors: Matthew Lund and Mohammad Javid
+ * Contact info: mtlund@ucsd.edu (Matthew),
+ *               Msamadpo@ucsd.edu (Mohammad)
  *
- * Author:
+ * Sources used:
+ *  1) Stepik: Introduction to Data Structures (Fall 2016)
+ *     by Moshiri and Izhikevich (available at stepik.org)
+ *
+ *     Section: 7.4 - Step 6
+ *
+ * Description of File:
+ *  This file defines the neccessary methods to create a
+ *  bit buffer to which we can write data. Once
+ *  the buffer has been completely filled with data,
+ *  the program flushes said data to an output specified
+ *  by the user.
+ *
  */
 #include "BitOutputStream.hpp"
 
-/* TODO */
+const unsigned int SIZE_OF_CHAR = 8;
+const unsigned int SET_FIRST_BIT = 0x01;
+
+/*
+ * This method writes all of the data that is found
+ * in the buffer to the output stream. This method
+ * is called when the buffer is full or when the
+ * user want to manually write the data contained
+ * in the buffer.
+ *
+ * Sources used:
+ *  1) Stepik: Introduction to Data Structures (Fall 2016)
+ *     by Moshiri and Izhikevich (available at stepik.org)
+ *
+ *     Section: 7.4 - Step 6
+ *
+ * Edge Case:
+ *  1) The buffer is empty -> return;
+ *
+ */
 void BitOutputStream::flush() {
+    // If the buffer is empty, don't flush.
     if (this->nbits == 0) {
         return;
     }
-    if (this->nbits == this->bufSize * 8) {
+    // If the buffer is full, flush all chars.
+    if (this->nbits == this->bufSize * SIZE_OF_CHAR) {
         this->out.write(this->buf, this->bufSize);
     } else {
-        int lastIndexOfCell = this->nbits / 8;
-        if (this->nbits % 8 == 0) {
+        // Get the char where nbit is.
+        int lastIndexOfCell = this->nbits / SIZE_OF_CHAR;
+        // Check if the whole char was filled, if so flush it.
+        if (this->nbits % SIZE_OF_CHAR == 0) {
             this->out.write(this->buf, lastIndexOfCell);
-        } else {
+        }
+        // If the whole char was not filled, add padding.
+        else {
             this->out.write(this->buf, lastIndexOfCell + 1);
         }
     }
+    // Clear the buffer.
     fill(this->buf, this->buf + this->bufSize, 0);
     this->nbits = 0;
 }
 
-/* TODO */
+/*
+ * This method writes an unsigned int into an unfilled bit
+ * in the buffer. If the buffer is full, the method is first
+ * going to flush the buffer and then write.
+ *
+ * Sources used:
+ *  1) Stepik: Introduction to Data Structures (Fall 2016)
+ *     by Moshiri and Izhikevich (available at stepik.org)
+ *
+ *     Section: 7.4 - Step 6
+ *
+ * Parameters:
+ *  1) i - The data which we want the next empty bit to contain.
+ *
+ * Edge Case:
+ *  1) The buffer is full -> flush before writting
+ *
+ */
 void BitOutputStream::writeBit(unsigned int i) {
-    if (this->nbits == (this->bufSize * 8)) {
+    // If we have read the whole buffer.
+    if (this->nbits == (this->bufSize * SIZE_OF_CHAR)) {
+        // Clear the buffer.
         this->flush();
         this->nbits = 0;
     }
+    // If we want to add a zero.
     if ((unsigned int)i == 0) {
         this->nbits++;
         return;
     }
-    int filledCells = this->nbits / 8;
-    int indexInCell = (this->nbits + 1) % 8;
+    // Pos of character where the nbit is located.
+    int filledCells = this->nbits / SIZE_OF_CHAR;
+    // Pos of the nbit inside the character.
+    int indexInCell = (this->nbits + 1) % SIZE_OF_CHAR;
+    // Get char where the nbit is.
     unsigned char ofCell = this->buf[filledCells];
+    // If we are at the last bit in the char, add 1.
     if (indexInCell == 0) {
-        ofCell = ofCell + 0x01;
+        ofCell = ofCell + SET_FIRST_BIT;
     } else {
+        // Shift of input to postion of nbit.
         unsigned int ofInput = i;
-        ofInput = ofInput << 8 - indexInCell;
+        ofInput = ofInput << SIZE_OF_CHAR - indexInCell;
+        // Add the desired bit at postion of nbit.
         ofCell = ofCell + ofInput;
     }
+    // Add the new modified char at postion of nbit.
     this->buf[filledCells] = ofCell;
     this->nbits++;
 }
 
+/*
+ * This method is a deconstructor. It frees the memory
+ * used by the buffer by deleting the character array.
+ */
 BitOutputStream::~BitOutputStream() { delete[] this->buf; }
--- src/bitStream/output/BitOutputStream.hpp
+++ src/bitStream/output/BitOutputStream.hpp
@@ -1,7 +1,21 @@
 /**
- * TODO: file header
+ * Authors: Matthew Lund and Mohammad Javid
+ * Contact info: mtlund@ucsd.edu (Matthew),
+ *               Msamadpo@ucsd.edu (Mohammad)
  *
- * Author:
+ * Sources used:
+ *  1) Stepik: Introduction to Data Structures (Fall 2016)
+ *     by Moshiri and Izhikevich (available at stepik.org)
+ *
+ *     Section: 7.4 - Step 6
+ *
+ * Description of File:
+ *  This file defines the neccessary methods to create a
+ *  bit buffer to which we can write data. Once
+ *  the buffer has been completely filled with data,
+ *  the program flushes said data to an output specified
+ *  by the user.
+ *
  */
 #ifndef BITOUTPUTSTREAM_HPP
 #define BITOUTPUTSTREAM_HPP
@@ -13,7 +27,19 @@ typedef unsigned char byte;
 
 using namespace std;
 
-/** TODO: class header */
+/**
+ * The BitOutputStream class allows the user to create a
+ * buffer in order to store data. Once the buffer has been
+ * filled, the buffer outputs all of its stored data into
+ * the output stream. The class also provides a method
+ * for the user to manually flush the buffer.
+ *
+ * Instance variables:
+ *  1) out - The outstream where we are writting the data
+ *  2) buf - The array that stores the data
+ *  3) bufSize - The size of the buffer which stores the data
+ *  4) nbits - The location of the next empty bit position.
+ */
 class BitOutputStream {
   private:
     ostream& out;          // reference to the output stream to use
@@ -22,7 +48,16 @@ class BitOutputStream {
     unsigned int nbits;    // number of bits have been writen to buf
 
   public:
-    /* TODO: add function header and implement */
+    /*
+     * This method creates an the BitOutputStream.
+     * It initializes all of the instance variables,
+     * allocates space for the buffer and once the buffer
+     * has been allocated, it zeroes out all of the data.
+     *
+     * Parameters:
+     *  1) os - The outstream to which to write the data
+     *  2) bufSize - The desired size of our buffer.
+     */
     explicit BitOutputStream(ostream& os, unsigned int bufSize) : out(os) {
         this->bufSize = bufSize;
         this->buf = new char[bufSize];
@@ -30,12 +65,49 @@ class BitOutputStream {
         this->nbits = 0;
     };
 
-    /* TODO: add function header */
+    /*
+     * This method writes all of the data that is found
+     * in the buffer to the output stream. This method
+     * is called when the buffer is full or when the
+     * user want to manually write the data contained
+     * in the buffer.
+     *
+     * Sources used:
+     *  1) Stepik: Introduction to Data Structures (Fall 2016)
+     *     by Moshiri and Izhikevich (available at stepik.org)
+     *
+     *     Section: 7.4 - Step 6
+     *
+     * Edge Case:
+     *  1) The buffer is empty -> return;
+     *
+     */
     void flush();
 
-    /* TODO: add function header */
+    /*
+     * This method writes an unsigned int into an unfilled bit
+     * in the buffer. If the buffer is full, the method is first
+     * going to flush the buffer and then write.
+     *
+     * Sources used:
+     *  1) Stepik: Introduction to Data Structures (Fall 2016)
+     *     by Moshiri and Izhikevich (available at stepik.org)
+     *
+     *     Section: 7.4 - Step 6
+     *
+     * Parameters:
+     *  1) i - The data which we want the next empty bit to contain.
+     *
+     * Edge Case:
+     *  1) The buffer is full -> flush before writting
+     *
+     */
     void writeBit(unsigned int i);
 
+    /*
+     * This method is a deconstructor. It frees the memory
+     * used by the buffer by deleting the character array.
+     */
     ~BitOutputStream();
 };
 
