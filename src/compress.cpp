5cd3d25de6c4613549d6a85223626740d4509374
Matthew Lund <50162103+mtlund@users.noreply.github.com>
2020-02-20 21:00:03 -0800

    Add files via upload

--- src/compress.cpp
+++ src/compress.cpp
@@ -1,7 +1,16 @@
 /**
- * TODO: file header
+ * Authors: Matthew Lund and Mohammad Javid
+ * Contact info: mtlund@ucsd.edu (Matthew),
+ *               Msamadpo@ucsd.edu (Mohammad)
  *
- * Author:
+ * Sources used:
+ *  1) CSE 100 Winter 2020, Lecture slides and discussion slides.
+ *
+ * Description of File:
+ * This file allows the user to take an input file and compress
+ * it into its encoded form. It allows the user to use true or
+ * ascii level encoding.
+ *
  */
 #include <fstream>
 #include <iostream>
@@ -12,47 +21,100 @@
 #include "HCTree.hpp"
 #include "cxxopts.hpp"
 
-/* TODO: add pseudo compression with ascii encoding and naive header
- * (checkpoint) */
+const unsigned int FILE_OUTPUT_POSITION = 2;
+const unsigned int FILE_INPUT_POSITION = 2;
+const unsigned int FILE_OUTPUT_POSITION_ASCII = 3;
+const unsigned int SET_SECOND_BIT = 0x02;
+const unsigned int SET_FIRST_BIT = 0x01;
+const unsigned int SIZE_OF_BUFFER = 4000;
+const unsigned int NUMBER_OF_CHARS = 256;
+
+/*
+ * This method allows the user to compress a file
+ * that using ascii level compression. It assumes that the
+ * user is utilizing a valid input file and has provided
+ * the name that the output file should have.
+ *
+ * Source: CSE 100 Lecture Slides and Discussion Slides.
+ *
+ * Parameters:
+ *  1) inFileName - the name of the input file.
+ *  2) outFileName - the name of the output file.
+ *
+ * Edge Cases:
+ *  1) All possible edge cases where accounted for in the main
+ *     method.
+ */
 void pseudoCompression(string inFileName, string outFileName) {
-    vector<unsigned int> ofChars(256);
+    // Creates a vector to store the frequency of each character.
+    vector<unsigned int> ofChars(NUMBER_OF_CHARS);
     HCTree* ofAllChars = new HCTree();
     ifstream ofInput;
     unsigned char nextChar;
     int nextByte;
     ofInput.open(inFileName, ios::binary);
+    // Loops through input file, counts the frequency of characters.
     while ((nextByte = ofInput.get()) != EOF) {
         nextChar = (unsigned char)nextByte;
         ofChars.at(nextChar)++;
     }
     ofInput.close();
+    // Build a huffman tree based on the frequencies.
     ofAllChars->build(ofChars);
     ofstream ofOutput;
     ofOutput.open(outFileName);
+    // Adds the frequency of each character to the header.
     for (int i = 0; i < ofChars.size(); i++) {
         unsigned int ofIndex = ofChars.at(i);
         ofOutput << ofIndex << endl;
     }
     ofInput.open(inFileName, ios::binary);
+    // Encodes each character in the input file.
     while ((nextByte = ofInput.get()) != EOF) {
         nextChar = (unsigned char)nextByte;
         ofAllChars->encode(nextChar, ofOutput);
     }
+    // Closes files used, frees memory used.
     ofInput.close();
     ofOutput.close();
     delete ofAllChars;
 }
 
-/* TODO: True compression with bitwise i/o and small header (final) */
+/*
+ * This method allows the user to compress a file
+ * that using "true" level compression. It assumes that the
+ * user is utilizing a valid input file and has provided
+ * the name that the output file should have.
+ *
+ * Source: CSE 100 Lecture Slides and Discussion Slides.
+ *
+ * Parameters:
+ *  1) inFileName - the name of the input file.
+ *  2) outFileName - the name of the output file.
+ *
+ * Edge Cases:
+ *  1) All possible edge cases where accounted for in the main
+ *     method.
+ */
 void trueCompression(string inFileName, string outFileName) {
-    vector<unsigned int> ofChars(256);
+    // Creates a vector to store the frequency of each character.
+    vector<unsigned int> ofChars(NUMBER_OF_CHARS);
     HCTree* ofAllChars = new HCTree();
     ifstream ofInput;
     unsigned char nextChar;
     int nextByte;
+    // Stores the number of characters in the input file.
     unsigned int totalChars = 0;
+    /*
+    / Character which outlines if the tree contains
+    / a new line character.
+    */
     unsigned char newLineRules = 0;
     ofInput.open(inFileName, ios::binary);
+    /*
+    / Loops through each character, counting its frequency
+    / and the total number of characters.
+    */
     while ((nextByte = ofInput.get()) != EOF) {
         nextChar = (unsigned char)nextByte;
         ofChars.at(nextChar)++;
@@ -61,40 +123,70 @@ void trueCompression(string inFileName, string outFileName) {
     ofInput.close();
     ofstream ofOutput;
     ofOutput.open(outFileName);
+    // Adds the number of characters in input file to header.
     ofOutput << totalChars << endl;
+    // Builds the huffman tree.
     ofAllChars->build(ofChars);
     auto ofHeader = new vector<char>();
+    // Adds characters to the header to represent the tree.
     ofAllChars->recursiveIteration(ofHeader, nullptr);
     std::reverse(ofHeader->begin(), ofHeader->end());
+    // Checks if the tree has a new line character.
     for (int i = 0; i < ofHeader->size(); i++) {
         if (ofHeader->at(i) == '\n') {
+            /*
+            / Checks to see if the new line character
+            / is the last one.
+            */
             if (i == ofHeader->size() - 1) {
-                newLineRules = newLineRules + 0x01;
+                // Sets bit representing the last character.
+                newLineRules = newLineRules + SET_FIRST_BIT;
             } else {
-                newLineRules = newLineRules + 0x02;
+                // Sets bit representing not the last character.
+                newLineRules = newLineRules + SET_SECOND_BIT;
             }
         }
     }
+    // Adds new line info to the header.
     ofOutput.put(newLineRules);
     ofOutput.put('\n');
+    // Adds the huffman tree representation to the header.
     for (int i = 0; i < ofHeader->size(); i++) {
         ofOutput.put(ofHeader->at(i));
     }
     ofOutput.put('\n');
     ofInput.open(inFileName, ios::binary);
-    BitOutputStream bos(ofOutput, 4000);
+    BitOutputStream bos(ofOutput, SIZE_OF_BUFFER);
+    // Encodes each character in input file, adds it to encoded file.
     while ((nextByte = ofInput.get()) != EOF) {
         nextChar = (unsigned char)nextByte;
         ofAllChars->encode(nextChar, bos);
     }
+    // Flushes buffer to make sure everything was written.
     bos.flush();
+    // Closes files used, frees memory used.
     ofInput.close();
     ofOutput.close();
     delete ofAllChars;
     delete ofHeader;
 }
 
-/* TODO: Main program that runs the compress */
+/*
+ * This handels the function calling and the interaction that
+ * the user has with the program. It provides the user with
+ * the required info for him/she to achieve the desired outcome.
+ * It accepts both ascii and true compression specifications.
+ *
+ * Source: CSE 100 PA3 Outline.
+ *
+ * Parameters:
+ *  1) argc - The number of inputs given by the user.
+ *  2) argv - The input of the user as strings.
+ *
+ * Edge Cases:
+ *  1) The files do not exists -> return
+ *  2) The input file is empty -> output empty file.
+ */
 int main(int argc, char* argv[]) {
     cxxopts::Options options("./compress",
                              "Compresses files using Huffman Encoding");
@@ -112,23 +204,29 @@ int main(int argc, char* argv[]) {
     options.parse_positional({"input", "output"});
     auto userOptions = options.parse(argc, argv);
 
+    // Checks if the input is valid or if the user wants help
     if (userOptions.count("help") || !FileUtils::isValidFile(inFileName) ||
         outFileName.empty()) {
         cout << options.help({""}) << std::endl;
         return 0;
     }
 
-    if (argv[3] == NULL) {
+    /*
+    / Checks if we want true compression
+    */
+    if (argv[FILE_OUTPUT_POSITION_ASCII] == NULL) {
+        // Checks if file is empty.
         if (FileUtils::isEmptyFile(argv[1])) {
             ofstream ofOutput;
-            ofOutput.open(argv[2], ios::trunc);
+            ofOutput.open(argv[FILE_OUTPUT_POSITION], ios::trunc);
             ofOutput.close();
             return 0;
         }
-        trueCompression(argv[1], argv[2]);
+        trueCompression(argv[1], argv[FILE_OUTPUT_POSITION]);
     } else {
-        string ofInputName = argv[2];
-        string ofOutputName = argv[3];
+        string ofInputName = argv[FILE_INPUT_POSITION];
+        string ofOutputName = argv[FILE_OUTPUT_POSITION_ASCII];
+        // Checks if file is empty.
         if (FileUtils::isEmptyFile(ofInputName)) {
             ofstream ofOutput;
             ofOutput.open(ofOutputName, ios::trunc);
--- src/uncompress.cpp
+++ src/uncompress.cpp
@@ -1,7 +1,17 @@
 /**
- * TODO: file header
+ * Authors: Matthew Lund and Mohammad Javid
+ * Contact info: mtlund@ucsd.edu (Matthew),
+ *               Msamadpo@ucsd.edu (Mohammad)
  *
- * Author:
+ * Sources used:
+ *  2) CSE 100 Winter 2020, Lecture slides and Discussion slides.
+ *
+ * Description of File:
+ * This file allows the user to take a compressed file
+ * and output its decoded from. It assumes that the
+ * user either compressed it with ascii or "true"
+ * compression style.
+ *
  */
 #include <fstream>
 #include <iostream>
@@ -11,72 +21,170 @@
 #include "HCTree.hpp"
 #include "cxxopts.hpp"
 
-/* TODO: Pseudo decompression with ascii encoding and naive header (checkpoint)
+const unsigned int FILE_OUTPUT_POSITION = 2;
+const unsigned int FILE_INPUT_POSITION = 2;
+const unsigned int FILE_OUTPUT_POSITION_ASCII = 3;
+const unsigned int VALUE_SECOND_BIT = 2;
+const unsigned int SIZE_OF_BUFFER = 4000;
+const unsigned int NUMBER_OF_CHARS = 256;
+
+/*
+ * This method allows the user to decompress an encoded file
+ * that used ascii level compression. It assumes that the
+ * user is utilizing a valid input file and has provided
+ * the name that the output file should have.
+ *
+ * Source: CSE 100 Lecture Slides and Discussion Slides.
+ *
+ * Parameters:
+ *  1) inFileName - the name of the input file.
+ *  2) outFileName - the name of the output file.
+ *
+ * Edge Cases:
+ *  1) All possible edge cases where accounted for in the main
+ *     method.
  */
 void pseudoDecompression(string inFileName, string outFileName) {
-    vector<unsigned int> ofChars(256);
+    // Stores the frequency of each character
+    vector<unsigned int> ofChars(NUMBER_OF_CHARS);
     HCTree* ofAllChars = new HCTree();
     ifstream ofInput;
     unsigned char nextChar;
     int nextByte;
+    /*
+    / Opens the file we want to decode,
+    / counts the frequency of each char.
+    */
     ofInput.open(inFileName, ios::binary);
-    for (int i = 0; i < 256; i++) {
+    for (int i = 0; i < NUMBER_OF_CHARS; i++) {
         ofInput >> ofChars.at(i);
     }
+    // Build the huffman tree.
     ofAllChars->build(ofChars);
+    // Creates or opens the file which to store the decode.
     ofstream ofOutput;
     ofOutput.open(outFileName, ios::trunc);
+    // Skips the \n char marking the end of the header.
     ofInput.get();
+    /*
+    / Loops through each character in the input file,
+    / finds the encoding belonging to the sequence of
+    / characters, adds the decoded character to output file.
+    */
     while ((nextByte = ofInput.peek()) != EOF) {
         ofOutput << ofAllChars->decode(ofInput);
     }
+    // Closes the files, frees memory.
     ofInput.close();
     ofOutput.close();
     delete ofAllChars;
 }
 
-/* TODO: True decompression with bitwise i/o and small header (final) */
+/*
+ * This method allows the user to decompress an encoded file
+ * that used "true" compression. It assumes that the
+ * user is utilizing a valid input file and has provided
+ * the name that the output file should have.
+ *
+ * Source: CSE 100 Lecture Slides and Discussion Slides.
+ *
+ * Parameters:
+ *  1) inFileName - the name of the input file.
+ *  2) outFileName - the name of the output file.
+ *
+ * Edge Cases:
+ *  1) All possible edge cases where accounted for in the main
+ *     method.
+ */
 void trueDecompression(string inFileName, string outFileName) {
+    // Initializes an empty tree.
     HCTree* ofAllChars = new HCTree();
+    // Vector stores the characters that make up the tree.
     vector<char>* ofHeader = new vector<char>();
     ifstream ofInput;
     int nextByte;
+    // Keeps track of the number of characters to decode.
     unsigned int totalChars = 0;
+    // Opens the file containing the encoded data.
     ofInput.open(inFileName, ios::binary);
+    // Gets the number of characters from the header.
     ofInput >> totalChars;
+    // Moves to next line.
     ofInput.get();
+    /*
+    / Gets a character which stores information about
+    / the possability that the huffman tree contains
+    / a new line character.
+    */
     unsigned int ofNewLineRules = ofInput.get();
+    // Moves to next line.
     ofInput.get();
+    /*
+    / Used to keep track of the current character
+    / we are at.
+    */
     unsigned char nextChar = ' ';
+    /*
+    / Loops through the characters representing the huffman
+    / tree until it hits the new line character.
+    */
     while ((nextChar = ofInput.get()) != '\n') {
         ofHeader->push_back(nextChar);
     }
+    // Checks if the tree contains a new line character.
     if (ofNewLineRules > 0) {
-        if (ofNewLineRules == 2) {
+        /*
+        / Checks to see if the tree continues past
+        / the new line character.
+        */
+        if (ofNewLineRules == VALUE_SECOND_BIT) {
+            /*
+            / Push the new line character that is part of the tree,
+            / continue to add characters that make up the tree.
+            */
             ofHeader->push_back(nextChar);
             while ((nextChar = ofInput.get()) != '\n') {
                 ofHeader->push_back(nextChar);
             }
         } else {
+            // Adds the last character that makes up the tree.
             ofHeader->push_back(nextChar);
             ofInput.get();
         }
     }
+    // Builds the tree recursively based on the header analyzed.
     ofAllChars->recursiveBuild(ofHeader, nullptr);
     ofstream ofOutput;
     ofOutput.open(outFileName);
-    BitInputStream bs(ofInput, 4000);
+    BitInputStream bs(ofInput, SIZE_OF_BUFFER);
+    // Decodes all of the bits in the encoding part of the file.
     while (totalChars > 0) {
         ofOutput << ofAllChars->decode(bs);
         totalChars--;
     }
+    // Closes files, frees the neccessary memory.
     ofInput.close();
     ofOutput.close();
     delete ofAllChars;
     delete ofHeader;
 }
 
-/* TODO: Main program that runs the uncompress */
+/*
+ * This handels the function calling and the interaction that
+ * the user has with the program. It provides the user with
+ * the required info for him/she to achieve the desired outcome.
+ * It accepts both ascii and true decompression.
+ *
+ * Source: CSE 100 PA3 Outline.
+ *
+ * Parameters:
+ *  1) argc - The number of inputs given by the user.
+ *  2) argv - The input of the user as strings.
+ *
+ * Edge Cases:
+ *  1) The files do not exists -> return
+ *  2) The input file is empty -> output empty file.
+ */
 int main(int argc, char* argv[]) {
     cxxopts::Options options("./uncompress",
                              "Uncompresses files using Huffman Encoding");
@@ -94,29 +202,36 @@ int main(int argc, char* argv[]) {
     options.parse_positional({"input", "output"});
     auto userOptions = options.parse(argc, argv);
 
+    // Checks if the input is valid or the user wants help.
     if (userOptions.count("help") || !FileUtils::isValidFile(inFileName) ||
         outFileName.empty()) {
         cout << options.help({""}) << std::endl;
         return 0;
     }
 
-    if (argv[3] == NULL) {
+    // Checks to see if we want true decoding.
+    if (argv[FILE_OUTPUT_POSITION_ASCII] == NULL) {
+        // Checks to see if the input file is empty.
         if (FileUtils::isEmptyFile(argv[1])) {
             ofstream ofOutput;
-            ofOutput.open(argv[2], ios::trunc);
+            ofOutput.open(argv[FILE_OUTPUT_POSITION], ios::trunc);
             ofOutput.close();
             return 0;
         }
-        trueDecompression(argv[1], argv[2]);
+        // Decompresses the file.
+        trueDecompression(argv[1], argv[FILE_OUTPUT_POSITION]);
     } else {
-        string ofInputName = argv[2];
-        string ofOutputName = argv[3];
+        // Assumes user wants ascii decoding.
+        string ofInputName = argv[FILE_INPUT_POSITION];
+        string ofOutputName = argv[FILE_OUTPUT_POSITION_ASCII];
+        // Checks if the input file is empty.
         if (FileUtils::isEmptyFile(ofInputName)) {
             ofstream ofOutput;
             ofOutput.open(ofOutputName, ios::trunc);
             ofOutput.close();
             return 0;
         }
+        // Decompresses the file.
         pseudoDecompression(ofInputName, ofOutputName);
     }
     return 0;
